083417e5102d6902a6238b462cf63b722fdbe7d6
Vesselin Russinov <v.russinov@gmail.com>
2013-02-16 15:36:47 +0200

    Added set_color and enemy_spotted.

--- robots/MilleniumFalcon.pm
+++ robots/MilleniumFalcon.pm
@@ -1,10 +1,18 @@
 #!/usr/local/bin/perl -w
 package MilleniumFalcon;
  our @ISA = "Tank";
+ my $color = "green";
  sub step{
    my $self = shift;
-   $self->set_color ( "green" );
+   $self->set_color ( $color );
    $self->turnRight(90);
    $self->move_forward(10);
+ }
+
+ sub enemy_spotted {
+	my $self = shift;
+	$color = "red";
+	$self->set_color( $color );
+	print "I SAW HIM! GONNA BLOW HIS... YOu GOT THE POINT!";
  }
 1;
